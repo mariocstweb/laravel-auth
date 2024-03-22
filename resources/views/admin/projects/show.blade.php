d016f9764350f60e533f9e56ec2cbec057f264cf
mariocstweb <mariocstmra@gmail.com>
2024-03-22 14:53:25 +0100

    fixed show.blade

--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -2,8 +2,9 @@
 
 @section('content')
  <h1 class="text-center p-3">{{$project->title}}</h1>
- <div class="container">
-  <div class="card">
+ <div class="container d-flex justify-content-center">
+  <div class="card" style="width: 18rem;">
+    <img src="{{$project->image}}" class="card-img-top p-3" alt="...">
     <div class="card-body">
       <h5 class="card-title">{{$project->title}}</h5>
       <p class="card-text">{{$project->content}}</p>
